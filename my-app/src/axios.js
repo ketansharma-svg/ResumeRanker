8f2f89041365a926940a0fbfe71b8dbbd4175e01
ketansharma-svg <ketan.sharma@w3eraa.com>
2026-01-08 15:24:31 +0530

    re-enter

--- my-app/src/axios.js
+++ my-app/src/axios.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production"
 console.log("Running in production?", isProduction)
 // Pick the base URL depending on environment
 const baseURL = isProduction
-  ? "https://resumeranker-jxh5.onrender.com"
+  ? "https://resumeranker-26gn.onrender.com"
   : "http://localhost:5000"
 
 console.log("Axios baseURL:", baseURL)
